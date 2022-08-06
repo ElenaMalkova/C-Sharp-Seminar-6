88100bed7733354d420b25f099751c2b25c8e7f1
Elena <malkovva@gmail.com>
2022-08-06 22:18:35 +0300

    Added solution to Task 2

---
+++ Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Method
+
+int CountPositiveNums(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int value = int.Parse(arr[i]);
+        if (value > 0) count++;
+    }
+    return count;
+}
+
+// Code
+
+Console.WriteLine("Введите несколько целых чисел через пробел");
+string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Количество введенных положительных чисел: {CountPositiveNums(array)}");
